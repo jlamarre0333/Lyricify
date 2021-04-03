7474c661602cd334a3cb970515741b06d4658378
Jake Lamarre <jakelamarre@hotmail.com>
2021-04-03 18:26:41 -0400

    added more api function and fixed html and added favorite lyrics page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
+const songInput = document.getElementById("song-input")
+const videoInput = document.getElementById("video-input")
+
 function getLyrics(artist, songname) {
     return fetch(`https://api.lyrics.ovh/v1/${artist}/${songname}`)
-    .then(res => res.json())
-    .then(data => data.lyrics)
-    .then(lyrics => console.log(lyrics))
+        .then(res => res.json())
+        .then(data => data.lyrics)
+        .then(lyrics => console.log(lyrics))
 }
 
+
+
 getLyrics('Drake', "What's Next")
+
+document.addEventListener('click', )
