cb431913602b95a3d6319fa7c1aaa406a4a3933a
Jake Lamarre <jakelamarre@hotmail.com>
2021-04-03 18:47:38 -0400

    added more logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,28 @@ function getLyrics(artist, songname) {
     return fetch(`https://api.lyrics.ovh/v1/${artist}/${songname}`)
         .then(res => res.json())
         .then(data => data.lyrics)
-        .then(lyrics => console.log(lyrics))
+        // .then(lyrics => console.log(lyrics))
 }
 
-
+var formSubmitHandler = function(event) {
+    // prevent page from refreshing
+    event.preventDefault();
+  
+    // get value from input element
+    // var songName = songInput.value.trim();
+  
+    if (songName) {
+      getLyrics(songName);
+      document.appendChild("div")
+  
+      // clear old content
+    //   repoContainerEl.textContent = '';
+    //   nameInputEl.value = '';
+    } else {
+      alert('Please enter a GitHub username');
+    }
+  };
 
 getLyrics('Drake', "What's Next")
 
-document.addEventListener('click', )
+document.addEventListener('submit', formSubmitHandler )
