08b0db5b29e8c4102819cb2102639bd8a1ea6021
xtrannguyen <tnguyenmw@gmail.com>
2021-04-07 08:21:13 -0400

    changed parameters

--- assets/js/ytscript.js
+++ assets/js/ytscript.js
@@ -1,8 +1,13 @@
- var getYoutubeVideos = function (search) {
-    
+ var artistEl = document.querySelector("#artist-input");
+ var songEl = document.querySelector("#song-input");
+ var buttonEl = document.querySelector("#search-button");
+ var videoListEl = document.querySelector("#videoListPlaceholder");
+ 
+ var getYoutubeVideos = function (artist, song) {
+    var search = artist + song;
     var addSearch = search.split(" ").join("+");
 
-    // grabbing whatever user searches
+    // grabbing user searches
     var apiURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + addSearch + "&order=viewCount&key=AIzaSyDfubMGVZns1rWCJ_VTsHqjAoYaa2PKyDw";
 
     // making a request to url
