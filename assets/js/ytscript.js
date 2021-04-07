093fcc331a82f69181b59a403bebfaeca0747ca7
xtrannguyen <tnguyenmw@gmail.com>
2021-04-07 07:02:59 -0400

    fetched api

--- assets/js/ytscript.js
+++ assets/js/ytscript.js
@@ -1,15 +1,25 @@
-var searchButtonEl = document.querySelector("#search-button");
-
-function ytInfo(){
-    gapi.client.setApiKey('AIzaSyDfubMGVZns1rWCJ_VTsHqjAoYaa2PKyDw');
-    gapi.client.load("youtube", "v3", function(){
-
-    });
-
+//     return fetch('https://www.googleapis.com/youtube/v3/search?part=snippet
+//     &forMine=true
+//     &q=Youtube+Data+API
+//     &order=viewCount
+//     &type=video
+//     &key=AIzaSyDfubMGVZns1rWCJ_VTsHqjAoYaa2PKyDw
+//     ')
+// };
  // api key does not require user authorization
  // key = 'AIzaSyDfubMGVZns1rWCJ_VTsHqjAoYaa2PKyDw';
  // searchURL = 'https://www.googleapis.com/youtube/v3/search';
-}
 
-var video = document.createElement("script");
-    video.src = "https://www.googleapis.com/youtube/v3/search"
+ var getYoutubeVideos = function () {
+    var response = fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDfubMGVZns1rWCJ_VTsHqjAoYaa2PKyDw')
+        .then(function(response){
+            response.json()
+            
+            .then(function(data){
+                console.log(data);
+            })
+        });
+
+};
+
+ getYoutubeVideos();
