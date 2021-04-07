a16b84e9d11747fbc1b6f3da48c64d60fed059b9
Jake Lamarre <jakelamarre@hotmail.com>
2021-04-07 16:16:40 -0400

    fixed formatting

--- assets/js/ytscript.js
+++ assets/js/ytscript.js
@@ -2,7 +2,7 @@ var artistEl = document.querySelector("#artist-input");
 var songEl = document.querySelector("#song-input");
 var buttonEl = document.querySelector("#search-button");
 var videoListEl = document.querySelector("#videoListPlaceholder");
- 
+
 var getYoutubeVideos = function (artist, song) {
     var search = artist + " " + song;
     var addSearch = search.split(" ").join("+");
@@ -11,16 +11,16 @@ var getYoutubeVideos = function (artist, song) {
     var apiURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + addSearch + "&order=viewCount&key=AIzaSyDfubMGVZns1rWCJ_VTsHqjAoYaa2PKyDw";
     // apiurl sample
     // https://www.googleapis.com/youtube/v3/search?part=snippet&q=john+legend+all+of+me&order=viewCount&key=AIzaSyDfubMGVZns1rWCJ_VTsHqjAoYaa2PKyDw
-    
+
     // making a request to url
-    fetch(apiURL).then(function(response){
-            response.json().then(function(data){
-                displayYtVideos(data, search);
-            });
+    fetch(apiURL).then(function (response) {
+        response.json().then(function (data) {
+            displayYtVideos(data, search);
         });
+    });
 };
 
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
 
     var artistInput = artistEl.value;
@@ -32,30 +32,30 @@ var formSubmitHandler = function(event) {
         songEl.value = "";
     }
     //else {
-        // figure out a way to display invalid search without alerts
+    // figure out a way to display invalid search without alerts
     //    alert("Try again");
-   // }
+    // }
 };
 
 var displayYtVideos = function (vidList, searchTerm) {
-    
-     // grabbing div named videoContainers
-     var videoContainerEl = document.querySelector("#videosContainer");
 
-     var vidLinkEmb = "https://www.youtube.com/embed/" + vidList.items[0].id.videoId;
-     
-     // creating iframe element
-     var playerFrameEl = document.createElement("iframe"); 
-            // customizing attributes to iframe
-            // shorten this by creating an object array
-            playerFrameEl.setAttribute("src", vidLinkEmb);
-            playerFrameEl.setAttribute("height", "360");
-            playerFrameEl.setAttribute("width", "480");
-            playerFrameEl.setAttribute("frameborder", "0");
-            playerFrameEl.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+    // grabbing div named videoContainers
+    var videoContainerEl = document.querySelector("#videosContainer");
 
-     // add to videoContainers div
-     videoContainerEl.appendChild(playerFrameEl); 
+    var vidLinkEmb = "https://www.youtube.com/embed/" + vidList.items[0].id.videoId;
+
+    // creating iframe element
+    var playerFrameEl = document.createElement("iframe");
+    // customizing attributes to iframe
+    // shorten this by creating an object array
+    playerFrameEl.setAttribute("src", vidLinkEmb);
+    playerFrameEl.setAttribute("height", "360");
+    playerFrameEl.setAttribute("width", "480");
+    playerFrameEl.setAttribute("frameborder", "0");
+    playerFrameEl.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+
+    // add to videoContainers div
+    videoContainerEl.appendChild(playerFrameEl);
 }
 
 buttonEl.addEventListener("click", formSubmitHandler);
