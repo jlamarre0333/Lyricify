3f372d6c7e6f4458bb5d15b50c9276fb38b0b29d
Jake Lamarre <jakelamarre@hotmail.com>
2021-04-07 19:02:13 -0400

    fixed api key

--- assets/js/ytscript.js
+++ assets/js/ytscript.js
@@ -8,7 +8,7 @@ var getYoutubeVideos = function (artist, song) {
     var addSearch = search.split(" ").join("+");
 
     // grabbing user searches
-    var apiURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + addSearch + "&order=viewCount&key=AIzaSyDfubMGVZns1rWCJ_VTsHqjAoYaa2PKyDw";
+    var apiURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + addSearch + "&order=viewCount&key=AIzaSyDLnpjvEUdZssvPFecwf4f88nTwEGH6eCM";
     // apiurl sample
     // https://www.googleapis.com/youtube/v3/search?part=snippet&q=john+legend+all+of+me&order=viewCount&key=AIzaSyDfubMGVZns1rWCJ_VTsHqjAoYaa2PKyDw
 
